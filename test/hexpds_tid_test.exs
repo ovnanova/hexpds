60e308f7f57b9bf438494f1869946a200e52804a
Shreyan Jain <shreyan.jain27@shreyan-jain27.local>
2024-01-31 12:33:16 -0800

    add failing test for converting Tid from string

--- test/hexpds_tid_test.exs
+++ test/hexpds_tid_test.exs
@@ -6,4 +6,8 @@ defmodule HexpdsTidTest do
     assert Hexpds.Tid.to_string(%Hexpds.Tid{timestamp: 1706732639995761, clock_id: 243}) == "3kkciofrlvlbn"
   end
 
+  test "converts from string" do
+    assert Hexpds.Tid.from_string("3kkciofrlvlbn") == %Hexpds.Tid{timestamp: 1706732639995761, clock_id: 243}
+  end
+
 end
