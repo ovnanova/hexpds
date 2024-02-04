92d4e3ad3746feb6451880c87dff079e7d14dfe0
hexanova <146671001+hexanova@users.noreply.github.com>
2024-02-03 20:19:27 -0800

    update k256, rip in peace from_be_bytes we will miss u

--- lib/hexpds/k256.ex
+++ lib/hexpds/k256.ex
@@ -1,4 +1,5 @@
 defmodule Hexpds.K256 do
   use Rustler, otp_app: :hexpds, crate: "hexpds_k256"
     def create_public_key(private_key), do: :erlang.nif_error(:nif_not_loaded)
+    def compress_public_key(public_key), do: :erlang.nif_error(:nif_not_loaded)
 end
