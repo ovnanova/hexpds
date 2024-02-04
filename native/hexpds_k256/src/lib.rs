5c946f9cf5438f4c62872be6c0468a5be742a7a1
hexanova <146671001+hexanova@users.noreply.github.com>
2024-02-03 21:29:53 -0800

    Gestaltzerfall

--- native/hexpds_k256/src/lib.rs
+++ native/hexpds_k256/src/lib.rs
@@ -25,13 +25,13 @@ fn create_public_key<'a>(env: Env<'a>, private_key: Binary<'a>) -> Term<'a> {
 }
 
 #[rustler::nif]
-fn compress_public_key<'a>(env: Env<'a>, public_key: Binary<'a>) -> Term<'a> {
-    let public_key = match hex::decode(public_key.as_slice()) {
-        Ok(key) => key,
+fn compress_public_key<'a>(env: Env<'a>, public_key_hex: Binary<'a>) -> Term<'a> {
+    let public_key_bytes = match hex::decode(public_key_hex.as_slice()) {
+        Ok(bytes) => bytes,
         Err(e) => return (atoms::error(), format!("Failed to decode hex string: {}", e)).encode(env),
     };
 
-    let public_key = match PublicKey::from_sec1_bytes(&public_key) {
+    let public_key = match PublicKey::from_sec1_bytes(&public_key_bytes) {
         Ok(key) => key,
         Err(e) => return (atoms::error(), format!("Failed to parse public key: {}", e)).encode(env),
     };
@@ -41,3 +41,4 @@ fn compress_public_key<'a>(env: Env<'a>, public_key: Binary<'a>) -> Term<'a> {
 }
 
 rustler::init!("Elixir.Hexpds.K256", [create_public_key, compress_public_key]);
+
