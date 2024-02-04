6ac9a27f7e1f6fe1d76c4b145986ef7f06ec69bd
Shreyan Jain <shreyan.jain.9@outlook.com>
2024-02-03 21:59:48 -0800

    did:key generation (#5)
    * add test cases for did:key generation
    
    * Encode did:key: according to spec
    
    * raising exceptions since it's a bit cleaner here
    
    * Rustler implementation
    
    * public key creation logic
    
    * input needs to be binary
    
    * uhhhhh rust things
    
    * update k256, rip in peace from_be_bytes we will miss u
    
    * FATAL ERROR
    
    A FATAL EXCEPTION HAS OCCURRED AT 0023:512000116958 IN LSTR-SYS-CE757026. THIS MAY BE CAUSED BY A HARDWARE FAILURE. IF THIS PROBLEM PERSISTS, CONTACT A REPLIKA TECHNICAL SERVICE. ERROR CODE ***STOP: 1X9EA4BB56 [LSTR-512]  PRESS ANY KEY TO REBOOT REPLIKA LOGIC
    
    * Gestaltzerfall
    
    * run it back
    
    * remove extra secp256k1 deps
    
    ---------
    
    Co-authored-by: hexanova <146671001+hexanova@users.noreply.github.com>

--- lib/hexpds/didgenerator.ex
+++ lib/hexpds/didgenerator.ex
@@ -9,12 +9,25 @@ defmodule Hexpds.DidGenerator do
   def get_public_key(privkey) when is_binary(privkey) and byte_size(privkey) == 32 do
     case(K256.create_public_key(privkey)) do
       {:ok, pubkey} -> pubkey
-      _ -> {:error, "Invalid private key"}
+      _ -> raise "Error generating public key"
     end
   end
 
-  def create_did_web_pubkey(pubkey) do
-    # Convert public key to the required format and encode
+  @spec multicodec_encode(binary(), :"secp256k1-pub") :: <<_::16, _::_*8>>
+  def multicodec_encode(pubkey, :"secp256k1-pub") do
+    <<0xE7, 0x01, pubkey::binary>>
+  end
+
+  @spec create_public_did_key(binary()) :: String.t()
+  def create_public_did_key(pubkey) do
+    "did:key:" <>
+      (case pubkey |> K256.compress_public_key() do
+         {:ok, pubkey} -> pubkey
+         _ -> raise "Error compressing public key"
+       end
+       |> Base.decode16!(case: :lower)
+       |> multicodec_encode(:"secp256k1-pub")
+       |> Multibase.encode!(:base58_btc))
   end
 
   def sign_genesis(genesis, privkey) do
---
+++ test/hexpds_didgenerator_test.exs
@@ -0,0 +1,28 @@
+defmodule HexpdsDidGeneratorTest do
+  use ExUnit.Case
+  doctest Hexpds.DidGenerator
+
+  defp test_cases do
+    %{
+      "9085d2bef69286a6cbb51623c8fa258629945cd55ca705cc4e66700396894e0c" =>
+        "did:key:zQ3shokFTS3brHcDQrn82RUDfCZESWL1ZdCEJwekUDPQiYBme",
+      "f0f4df55a2b3ff13051ea814a8f24ad00f2e469af73c363ac7e9fb999a9072ed" =>
+        "did:key:zQ3shtxV1FrJfhqE1dvxYRcCknWNjHc3c5X1y3ZSoPDi2aur2",
+      "6b0b91287ae3348f8c2f2552d766f30e3604867e34adc37ccbb74a8e6b893e02" =>
+        "did:key:zQ3shZc2QzApp2oymGvQbzP8eKheVshBHbU4ZYjeXqwSKEn6N",
+      "c0a6a7c560d37d7ba81ecee9543721ff48fea3e0fb827d42c1868226540fac15" =>
+        "did:key:zQ3shadCps5JLAHcZiuX5YUtWHHL8ysBJqFLWvjZDKAWUBGzy",
+      "175a232d440be1e0788f25488a73d9416c04b6f924bea6354bf05dd2f1a75133" =>
+        "did:key:zQ3shptjE6JwdkeKN4fcpnYQY3m9Cet3NiHdAfpvSUZBFoKBj"
+    }
+  end
+
+  test "derives correct did:key" do
+    for {privkey_hex, expected_did} <- test_cases() do
+      assert privkey_hex
+             |> Base.decode16!(case: :lower)
+             |> Hexpds.DidGenerator.get_public_key()
+             |> Hexpds.DidGenerator.create_public_did_key() == expected_did
+    end
+  end
+end
