5b8ab6f409eed9adcfa16be57433db01d36d7bd1
ɴᴏᴠᴀ <146671001+ovnanova@users.noreply.github.com>
2024-02-15 21:31:36 -0800

    serde_ipld_dagcbor crate NIF wrapper

---
+++ lib/hexpds/dagcbor.ex
@@ -0,0 +1,7 @@
+defmodule Hexdps.DagCBOR do
+  defmodule Internal do
+    use Rustler, otp_app: :hexpds, crate: "hexpds_dagcbor_internal"
+      @spec encode_dag_cbor(binary()) :: {:ok, binary()} | {:error, String.t()}
+      def encode_dag_cbor(_json), do: :erlang.nif_error(:nif_not_loaded)
+  end
+end
---
+++ native/hexpds_dagcbor_internal/src/lib.rs
@@ -0,0 +1,29 @@
+use rustler::Encoder;
+use rustler::Env;
+use rustler::Term;
+use serde_json::from_str;
+use serde_ipld_dagcbor::to_vec;
+
+mod atoms {
+    rustler::atoms! {
+        ok,
+        error,
+    }
+}
+
+#[rustler::nif]
+fn encode_dag_cbor(env: Env, json: String) -> Term {
+    let parsed_json = match from_str(&json) {
+        Ok(json) => json,
+        Err(e) => return (atoms::error(), format!("Failed to parse JSON: {}", e)).encode(env),
+    };
+
+    let cbor_data = match to_vec(&parsed_json) {
+        Ok(data) => data,
+        Err(e) => return (atoms::error(), format!("Failed to encode to DAG-CBOR: {}", e)).encode(env),
+    };
+
+    (atoms::ok(), cbor_data).encode(env)
+}
+
+rustler::init!("Elixir.Hexpds.DagCBOR.Internal", [encode_dag_cbor]);
