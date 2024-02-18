4d488efa8a1ed45bd77f58731d833409f45c415e
ShreyanJain9 <shreyan.jain.9@outlook.com>
2024-02-17 20:44:49 -0800

    doctest for cbor encoding

--- lib/hexpds/dagcbor.ex
+++ lib/hexpds/dagcbor.ex
@@ -6,6 +6,13 @@ defmodule Hexpds.DagCBOR do
       @spec decode_dag_cbor(binary()) :: {:ok, String.t()} | {:error, String.t()}
       def decode_dag_cbor(_cbor), do: :erlang.nif_error(:nif_not_loaded)
   end
+  @doc """
+  Encodes a JSON string into a CBOR binary.
+
+  Example:
+    iex> Hexpds.DagCBOR.encode_json(Jason.encode!(%{apple: "banana", best_programming_language: "ruby"})) |> elem(1) |> to_charlist()
+    [162, 101, 97, 112, 112, 108, 101, 102, 98, 97, 110, 97, 110, 97, 120, 25, 98, 101, 115, 116, 95, 112, 114, 111, 103, 114, 97, 109, 109, 105, 110, 103, 95, 108, 97, 110, 103, 117, 97, 103, 101, 100, 114, 117, 98, 121]
+  """
   def encode_json(json) do
     with {:ok, cbor} <- Internal.encode_dag_cbor(json) do
          {:ok, to_string(cbor)}
